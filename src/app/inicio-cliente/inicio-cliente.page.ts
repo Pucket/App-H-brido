e062351fb9489d547fb5971b76881d9b1ac73755
Pucket <adilson.oliveira.ao3@gmail.com>
2021-01-08 18:41:54 -0300

    acesso apenas depois de logado

--- src/app/inicio-cliente/inicio-cliente.page.ts
+++ src/app/inicio-cliente/inicio-cliente.page.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
+import { UsuarioService } from 'src/app/services/usuario.service'
+import { AutenticacaoclienteService } from 'src/app/services/autenticacaocliente.service'
+import { NavController } from '@ionic/angular';
+
 
 @Component({
   selector: 'app-inicio-cliente',
@@ -8,9 +12,75 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InicioClientePage implements OnInit {
 
-  constructor() { }
+  usuario: any;
+
+  email: string;
+
+  constructor(private service: UsuarioService,
+              private autenticacao: AutenticacaoclienteService,
+              private nav: NavController
+              ) { }
 
   ngOnInit() {
+
+    let email: string;
+
+
+
+    this.autenticacao.detalhes().subscribe(res => {
+
+      email = res.email;
+
+    }, err => {
+
+     console.log('err', err);
+
+    });
+
+    this.service.listar().subscribe(data => {
+
+      this.usuario = data.map(e => {
+
+        return{
+
+          id: e.payload.doc.id,
+
+          nome: e.payload.doc.data()['nome'],
+
+          email: e.payload.doc.data()['email'],
+
+          telefone: e.payload.doc.data()['telefone'],
+
+          endereco: e.payload.doc.data()['endereco']
+
+        };
+
+      });
+
+      console.log(this.usuario);
+
+      console.log(email);
+
+      for (let it of this.usuario){
+
+        console.log(email!==it.email);
+
+        if (email!==it.email) {
+
+          console.log("Encontrou");
+
+          this.nav.navigateForward('login-cliente');
+          
+          break;
+
+          
+
+        }
+
+      }
+
+    }); 
+
   }
 
 }
