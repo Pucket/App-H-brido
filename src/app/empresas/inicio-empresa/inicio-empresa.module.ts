870501505a7208bb7067bb83684e8ba1b4054f1f
joyce figueira <joycekerry90@gmail.com>
2021-01-08 21:26:53 -0300

    feito empresa

--- src/app/empresas/inicio-empresa/inicio-empresa.module.ts
+++ src/app/empresas/inicio-empresa/inicio-empresa.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
 
@@ -13,7 +13,9 @@ import { InicioEmpresaPage } from './inicio-empresa.page';
     CommonModule,
     FormsModule,
     IonicModule,
-    InicioEmpresaPageRoutingModule
+    InicioEmpresaPageRoutingModule,
+    ReactiveFormsModule
+
   ],
   declarations: [InicioEmpresaPage]
 })
