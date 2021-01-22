d9d61694beaaa45d45396466cbbad8bf88c4f915
Erasmo02 <eerasminho54@gmail.com>
2021-01-22 18:48:38 -0300

    layaut exibir serviços

--- src/app/exibir-servico/exibir-servico.page.ts
+++ src/app/exibir-servico/exibir-servico.page.ts
@@ -14,6 +14,7 @@ export class ExibirServicoPage implements OnInit {
   nome: string;
   descricao: string;
   valor: string;
+  email: string;
 
   constructor(private nav: NavController,
               private rota: ActivatedRoute) { }
@@ -24,6 +25,7 @@ export class ExibirServicoPage implements OnInit {
     this.nome = this.rota.snapshot.params['nome'];
     this.descricao = this.rota.snapshot.params['descricao'];
     this.valor = this.rota.snapshot.params['valor'];
+    this.valor = this.rota.snapshot.params['email'];
 
   }
 
